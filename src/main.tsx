429a0bff9128a934ba687aae377c478cbe5ade00
Eskil Magnussen <eskil.magnussen@gmail.com>
2025-07-23 14:54:14 +0200

    enabled analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Portfolio from './pages/Portfolio.tsx'
 import './index.css'
+import { Analytics } from "@vercel/analytics/next"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Portfolio />
+    <Analytics />
   </React.StrictMode>,
 )
