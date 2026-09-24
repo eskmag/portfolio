7931c1a6d2217a6583a4ae855286f679580db182
Eskil Magnussen <eskil.magnussen@gmail.com>
2026-09-24 23:35:01 +0200

    fix: small detail fix

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -15,7 +15,7 @@ export const projects: Project[] = [
     description: "A web-app showcasing the potential for emergency water supply from rainwater collection in Bergen.",
     link: "https://github.com/eskmag/bergen-smart-rain-hub",
     deployed_link: "https://bergen-smart-rain-hub.onrender.com",
-    technologies: ["Python", "Pandas", "SQLite", "Streamlit"],
+    technologies: ["Python", "Pandas", "SQLite", "Tailwind CSS", "Typescript"],
     tag: "Climate",
     featured: true,
   },
